901c4238dac96cdc193207f6042ec71554884065
dduartess <dandandsoares@gmail.com>
2026-01-22 11:12:44 -0300

    Feat: create Product model and JPA mapping

---
+++ src/main/java/com/example/Product_Management/model/Product.java
@@ -0,0 +1,72 @@
+package com.example.Product_Management.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nameProduct;
+    private String descriptionProduct;
+    private Double priceProduct;
+
+    public Product(){
+
+    }
+
+    public Product(String nameProduct, String descriptionProduct, Double priceProduct) {
+        this.nameProduct = nameProduct;
+        this.descriptionProduct = descriptionProduct;
+        this.priceProduct = priceProduct;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public String getDescriptionProduct() {
+        return descriptionProduct;
+    }
+
+    public void setDescriptionProduct(String descriptionProduct) {
+        this.descriptionProduct = descriptionProduct;
+    }
+
+    public Double getPriceProduct() {
+        return priceProduct;
+    }
+
+    public void setPriceProduct(Double priceProduct) {
+        this.priceProduct = priceProduct;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
