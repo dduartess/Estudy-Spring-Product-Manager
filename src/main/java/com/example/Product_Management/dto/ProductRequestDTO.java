ecafeee9deee3b5fe711c84d7e8f2c6fdc2a5264
dduartess <dandandsoares@gmail.com>
2026-01-22 15:16:04 -0300

    Refactor: Change types in DTO's

--- src/main/java/com/example/Product_Management/dto/ProductRequestDTO.java
+++ src/main/java/com/example/Product_Management/dto/ProductRequestDTO.java
@@ -1,9 +1,7 @@
 package com.example.Product_Management.dto;
 
-import java.math.BigDecimal;
-
 public record ProductRequestDTO(
         String nameProduct,
         String descriptionProduct,
-        BigDecimal priceProduct
+        Double priceProduct
 ) {}
--- src/main/java/com/example/Product_Management/dto/ProductResponseDTO.java
+++ src/main/java/com/example/Product_Management/dto/ProductResponseDTO.java
@@ -1,11 +1,10 @@
 package com.example.Product_Management.dto;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 public record ProductResponseDTO(
         UUID id,
         String name,
         String description,
-        BigDecimal priceProduct
+        Double priceProduct
 ) {}
