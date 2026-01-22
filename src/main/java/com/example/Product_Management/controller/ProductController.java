97ef2e63b1a169f4e514d38db128ad21c8ca7a17
dduartess <dandandsoares@gmail.com>
2026-01-22 15:16:56 -0300

    Refactor: Organize importes

--- src/main/java/com/example/Product_Management/controller/ProductController.java
+++ src/main/java/com/example/Product_Management/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.example.Product_Management.controller;
 import com.example.Product_Management.dto.ProductRequestDTO;
 import com.example.Product_Management.dto.ProductResponseDTO;
 import com.example.Product_Management.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
