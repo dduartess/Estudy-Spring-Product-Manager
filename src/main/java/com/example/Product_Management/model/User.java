2d417f4a993b3f4a09ce113c18a074d397bf84a5
dduartess <dandandsoares@gmail.com>
2026-01-23 15:31:08 -0300

    Feat: Create User and Role entity and Enum

---
+++ src/main/java/com/example/Product_Management/model/User.java
@@ -0,0 +1,54 @@
+package com.example.Product_Management.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(UUID id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/Product_Management/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.example.Product_Management.model.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
