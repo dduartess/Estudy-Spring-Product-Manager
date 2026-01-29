4935a08ed5f26c68feedccfac0dee870ac1e9601
_dduartess <dandandsoares@gmail.com>
2026-01-29 11:11:35 -0300

    Feat: create methods in UserRepository

--- src/main/java/com/example/Product_Management/repository/UserRepository.java
+++ src/main/java/com/example/Product_Management/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.Product_Management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.Product_Management.model.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    UserDetails findByLogin(String login);
+
+    String login(String login);
 }
